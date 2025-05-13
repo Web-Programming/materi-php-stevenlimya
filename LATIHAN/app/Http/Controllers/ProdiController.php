ce350605ab7ba9f1d37935772789681a1ab8c394
stevenlim <stevenlimya@gmail.com>
2025-05-13 20:37:56 +0700

    latihan

--- LATIHAN/app/Http/Controllers/ProdiController.php
+++ LATIHAN/app/Http/Controllers/ProdiController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use View;
 
 class ProdiController extends Controller
 {
     //
+}
+
+public function index(): View{
+    $listprodi = prodi::get()
+    return view ("prodi.index");
+}
+public function create():{
+    return view
+
+
 }
