533fd69559fc1f0c5e83aead226bb8a7af222e90
stevenlim <stevenlimya@gmail.com>
2025-05-17 18:21:58 +0700

    latihan

--- LATIHAN/app/Http/Controllers/ProdiController.php
+++ LATIHAN/app/Http/Controllers/ProdiController.php
@@ -2,9 +2,75 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Prodi;
 use Illuminate\Http\Request;
 
 class ProdiController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $listprodi = Prodi::get();
+        return view("prodi.index",['listprodi' => $listprodi]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+         return view("prodi.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'nama' => 'required|min:5|max:20',
+            'kode_prodi' => 'required|min:2|max:2'
+            ]);
+
+            $prodi = new Prodi();
+            $prodi->nama = $validateData['nama'];//
+            $prodi->kode_prodi = $validateData['kode_prodi'];
+            $prodi->save();
+
+            return redirect("prodi")->with("status","Data Program Studi Berhasil Disimpan");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
--- LATIHAN/routes/web.php
+++ LATIHAN/routes/web.php
@@ -21,13 +21,11 @@ Route::get('/', function () {
 Route::get('/beranda', function () {
     return view('beranda', 
     [
-        'name' => 'Angel',
-        'email' => 'angelstvny10@gmail.com',
+        'name' => 'StevenLim',
+        'email' => 'stevenlimya@gmail.com',
         'alamat' => 'Palembang']);
 });
 
 Route::get('/berita/', function ($id, $judul = null) {
     return view('berita', ['id' => $id, 'judul' => $judul]);
 });
-
-Route::get('/prodi/index', [ProdiController::class,'index']);
